8a4e708f36139a788bc3b782f88dd6bc67717a38
myrat012 <erlekow1997@gmail.com>
2021-07-12 12:30:26 +0500

    update

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Blog Page</h1>")
+    context = {
+        'k_post': Post.objects.all()
+    }
+    return render(request, "blog/home.html", context)
+
+def about(request):
+    return render(request, "blog/about.html", {'title': 'About'})
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'blog.apps.BlogConfig',
+
+    'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'    #name url pattern
+LOGIN_URL = 'login'
